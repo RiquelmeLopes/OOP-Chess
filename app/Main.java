444bc76aa6fe9f4bec726a8893d68b7b19060ea4
RiquelmeLopes <73597510+RiquelmeLopes@users.noreply.github.com>
2021-08-07 14:29:50 -0300

    Update Main.java

--- app/Main.java
+++ app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 import java.util.Scanner;
 
+import xadrez.Cor;
 import xadrez.XadrezMatch;
 import xadrez.XadrezPeca;
 import xadrez.XadrezPosicao;
@@ -9,31 +10,40 @@ import xadrez.XadrezPosicao;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
+		boolean jogando = true;
 		String jogador1;
-		String jogador2
-			
+		String jogador2;
+
 		Scanner sc = new Scanner(System.in);
-		XadrezMatch xadrezMatch  = new XadrezMatch();
-		
+		XadrezMatch xadrezMatch = new XadrezMatch();
+
 		System.out.println("Jogador 1, digite seu nome :");
 		jogador1 = sc.nextLine();
 		System.out.println("Jogador 2, digite seu nome :");
 		jogador2 = sc.nextLine();
-		
-		while (true) {	
-		UI.printTabuleiro(xadrezMatch.getPecas());
-		System.out.println();
-		System.out.print(jogador1 + " , digite a peça que quer mover : ");
-		XadrezPosicao source = UI.readXadrezPosicao(sc);
-		
-		boolean[][] possibleMoves = xadrezMatch.possibleMoves(source);
-		
-		System.out.println();
-		System.out.print(jogador1 + ", digite para onde quer mover a peça : ");
-		XadrezPosicao target = UI.readXadrezPosicao(sc);
 
-		XadrezPeca capturadaPeca = xadrezMatch.performChessMove(source, target);
-	}	
+		while (jogando) {
+
+			UI.printTabuleiro(xadrezMatch.getPecas());
+			System.out.println();
+			if (xadrezMatch.getCurrentPlayer() == Cor.WHITE) {
+				System.out.print(jogador1 + " , digite seu movimento : (caso queira desistir, faça um movimento invalido) ");
+			} else
+				System.out.print(jogador2 + " , digite seu movimento : (caso queira desistir, faça um movimento invalido) ");
+
+			String movimento = sc.nextLine();
+			String s[] = movimento.split(":");
+			String movOrigem = s[0];
+			String movDestino = s[1];
+
+			XadrezPosicao source = UI.readXadrezPosicao(movOrigem);
+			boolean[][] possibleMoves = xadrezMatch.possibleMoves(source);
+
+			XadrezPosicao target = UI.readXadrezPosicao(movDestino);
+
+			XadrezPeca capturadaPeca = xadrezMatch.performChessMove(source, target);
+		}
+		sc.close();
 	}
-	}
+}
